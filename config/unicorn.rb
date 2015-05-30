01db46a095f30f7652ae243f2ed0928e1bfcee03
Eli Shkurkin <eshkurkin@gmail.com>
2015-05-30 15:12:39 -0700

    change unicorn configt

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,5 @@
 # set path to application
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
 working_directory app_dir
 
 
@@ -10,11 +9,11 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen "tmp/unicorn.sock", :backlog => 64
 
 # Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+stderr_path "/var/log/unicorn.stderr.log"
+stdout_path "/var/log/unicorn.stdout.log"
 
 # Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+pid "/tmp/pids/unicorn.pid"
