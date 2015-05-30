9322617ff3d07bfce27fcce0ae6b2ee003214395
Eli Shkurkin <eshkurkin@gmail.com>
2015-05-30 16:02:59 -0700

    update unicorn sock path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,7 +9,7 @@ preload_app true
 timeout 30
 
 # Set up socket location
-listen "tmp/unicorn.sock", :backlog => 64
+listen "/tmp/unicorn.sock", :backlog => 64
 
 # Logging
 stderr_path "/var/log/unicorn.stderr.log"
