15e9d9e9c9b795abc5723182be37dbeeba41c584
Eli Shkurkin <eshkurkin@gmail.com>
2015-07-19 14:29:26 -0700

    fix team name generation bug

--- app/models/team.rb
+++ app/models/team.rb
@@ -6,14 +6,14 @@ class Team < ActiveRecord::Base
   validates_uniqueness_of :name
 
   def self.generate_name
-    names = ["Team Awesome", "Beagles", "The Canadians", "xxDestoryersxx",
-      "emancipators", "Foobars", "Germz", "The Haters", "Ions", "Ja Feel",
+    names = ["Team Awesome", "Beagles", "The Canadians", "xxDestoryerxx",
+      "Emancipators", "Foobars", "Germz", "The Haters", "Ions", "Ja Feel",
       "North Korea", "The Losers", "Meercats", "No Shows", "The OhEmGeez",
       "Porto Potties", "Qi", "Randy", "Stoopids", "Team", "Usefuls",
       "Vandals", "Whut", "XXX", "YoYos", "Zoombinis"]
-    name = names.sample + rand(1000...9999);
+    name = names.sample + rand(1000...9999).to_s;
     while(Team.find_by_name(name)) do
-      name = names.sample + rand(1000...9999);
+      name = names.sample + rand(1000...9999).to_s;
     end
     name
   end
